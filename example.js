c34b45de0148d5d1c4d0aa031635f2beb33a777b
Marcelo de Bittencourt <tchelopoa@gmail.com>
2021-10-05 09:55:33 -0300

    Add view and slow motion to test execution

--- example.js
+++ example.js
@@ -2,7 +2,11 @@ const puppeteer = require('puppeteer');
 
 (async () => {
   //const browser = await puppeteer.launch();
-  const browser = await puppeteer.launch({ headless: false }); // default is true
+  //const browser = await puppeteer.launch({ headless: false }); // default is true
+  const browser = await puppeteer.launch({
+    headless: false,
+    slowMo: 250, // slow down by 250ms
+  })
   const page = await browser.newPage();
   await page.goto('https://example.com');
   await page.screenshot({ path: 'example.png' });
